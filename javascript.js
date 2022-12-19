22208ec47bb0a0d5c11377a1f54561e9b15e2248
Fantoooo <celian.lecorvoisier@orange.fr>
2022-12-19 23:32:42 +0100

    adding the function for  ; add remove multiplicati on and division

---
+++ javascript.js
@@ -0,0 +1,16 @@
+/*ajouter */
+function add(number1, number2){
+    return number1 + number2
+}
+/*soustraire */
+function remove(number1, number2){
+    return number1 - number2
+}
+/*multiplier */
+function multiplication(number1, number2){
+    return number1 * number2
+}
+/*diviser */
+function diviser(number1, number2){
+    return number1/number2
+}
