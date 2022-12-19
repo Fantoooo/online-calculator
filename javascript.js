d09924f85485b47c55e05394b0972587dbccfa42
Fantoooo <celian.lecorvoisier@orange.fr>
2022-12-19 23:39:20 +0100

    adding the function that with an operator do the good  action

--- javascript.js
+++ javascript.js
@@ -14,3 +14,18 @@ function multiplication(number1, number2){
 function diviser(number1, number2){
     return number1/number2
 }
+/*fonction qui s'adapte */
+function operate(number1, number2, operateur){
+    if (operateur=="+"){
+        return add(number1, number2)
+    }
+    if (operateur=="-"){
+        return remove(number1, number2)
+    }
+    if (operateur=="*"){
+        return multiplication(number1, number2)
+    }
+    if (operateur=="/"){
+        return diviser(number1, number2)
+    }
+}
